d7060b787f383957cbc71208aae847ddfb1ff9f7
Eina Onting <einers@outlook.com>
2020-01-21 00:39:33 +0000

    receive POST requests from urls/new + install body-parser dependency

--- express_server.js
+++ express_server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
-// set view engine to ejs
-// app.set('views', './');
-app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
+// set view engine to ejs
+// app.set('views', './');
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.send("Hello!");  
@@ -21,6 +24,10 @@ app.get("/urls", (req, res) => {
   });
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL,
@@ -29,13 +36,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render('urls_show', templateVars);
 })
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// })
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
