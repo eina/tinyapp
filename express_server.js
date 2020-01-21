2e86ba079c06c30f74eeecf1e1941c0be19e2392
Eina Onting <einers@outlook.com>
2020-01-21 01:10:23 +0000

    implement shortURL redirect to longURL

--- express_server.js
+++ express_server.js
@@ -42,6 +42,12 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render('urls_show', templateVars);
 })
 
+app.get("/u/:shortURL", (req, res) => {  
+  console.log(req.params)
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);  
+})
+
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;  
