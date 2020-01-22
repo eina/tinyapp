1d37e2d3e0f4aab40ea07a59069b84618088ac5e
Eina Onting <einers@outlook.com>
2020-01-21 18:44:47 -0800

    create login page + update header buttons

--- express_server.js
+++ express_server.js
@@ -45,6 +45,10 @@ app.get("/register", (req, res) => {
   res.render("urls_register", {});
 });
 
+app.get("/login", (req, res) => {
+  res.render("urls_login", {});
+});
+
 app.get("/urls", (req, res) => {
   console.log("what are you", users[req.cookies.user_id]);
   res.render("urls_index", {
